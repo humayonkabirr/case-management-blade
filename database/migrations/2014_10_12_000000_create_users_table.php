9b197cd4803730a3c8eb65c976a74f4eec53cccd
humayonkabirr <humayonkobir8@gmail.com>
2024-10-15 17:39:02 +0600

    update user general info table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('id');
-            $table->string('name');
+            $table->string('first_name',30);
+            $table->string('last_name',30);
             $table->string('username')->nullable();
-            $table->text('avatar')->nullable();
-            $table->string('phone')->unique()->nullable();
-            $table->string('type')->nullable();
+            $table->string('mobile')->unique()->nullable();
             $table->string('email')->unique();
+            $table->date('birthday')->nullable();
+            $table->string('blood_group', 3)->nullable();
+            $table->string('religion', 15)->nullable();
+            $table->string('gender', 10)->nullable();
+            $table->string('nationality', 20)->nullable();
+            $table->string('mother_tongue', 20)->nullable();
+            $table->text('avatar')->nullable();
+            $table->string('type')->nullable();
+
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
