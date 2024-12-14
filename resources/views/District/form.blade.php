4a9c8d9a970497466a095d9c64670f735438e90b
humayonkabirr <humayonkobir8@gmail.com>
2024-12-14 16:18:13 +0600

    district view file delete

--- resources/views/District/form.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout.master')
-
-@section('content')
-    <h1>District Form</h1>
-@endsection
--- resources/views/District/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout.master')
-
-@section('content')
-    <h1>District Index</h1>
-@endsection
