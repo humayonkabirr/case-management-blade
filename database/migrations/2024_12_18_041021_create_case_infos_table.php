93878c95bc7d1f4e42c41c400a332bac2138b168
humayonkabirr <humayonkobir8@gmail.com>
2024-12-19 10:51:57 +0600

    case info migrations file update column

--- database/migrations/2024_12_18_041021_create_case_infos_table.php
+++ database/migrations/2024_12_18_041021_create_case_infos_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('case_infos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('case_type_id')->constrained()->onDelete('cascade')->nullable();
+            $table->unsignedBigInteger('advocate_id');
+			$table->foreign('advocate_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('Title')->uniqid();
-            $table->string('bn_Title')->nullable();  
-            $table->string('bn_location', 250)->nullable();
+            $table->string('bn_Title')->nullable();
             $table->unsignedInteger('serial')->default(1);
             $table->unsignedInteger('status')->default(1)->commit(['0=>Inactive, 1=>Active, 2=>Block, 3=>Band, 4=>Suspend']);
             $table->timestamps();
