6135b1bad2e79d37b9f5ba603471c3bdfa51b599
humayonkabirr <humayonkobir8@gmail.com>
2024-12-21 10:48:18 +0600

    update case info migration file update

--- app/Http/Controllers/backend/CaseInfoController.php
+++ app/Http/Controllers/backend/CaseInfoController.php
@@ -3,16 +3,37 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CaseInfoRequest;
+use App\Services\CaseInfoService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
 
 class CaseInfoController extends Controller
 {
+    protected $caseInfoService;
+
+    public function __construct(CaseInfoService $caseInfoService)
+    {
+        $this->caseInfoService  = $caseInfoService;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        try {
+            if (Gate::allows('dashboard.index')) {
+                $data['caseInfos'] = $this->caseInfoService->list()->paginate(15);
+                return view('backend.case-info.index', $data);
+            }
+            return view('errors.403');
+        } catch (\Throwable $e) {
+            $errorMessage = $e->getMessage(); // Define the error message variable
+            return view('errors.500', compact('errorMessage'));
+        }
     }
 
     /**
@@ -20,16 +41,48 @@ class CaseInfoController extends Controller
      */
     public function create()
     {
-        //
+        try {
+            if (Gate::allows('dashboard.index')) {
+                $data['caseInfos'] = $this->caseInfoService->list()->paginate(15);
+                return view('backend.case-info.form', $data);
+            }
+            return view('errors.403');
+        } catch (\Throwable $e) {
+            $errorMessage = $e->getMessage(); // Define the error message variable
+            return view('errors.500', compact('errorMessage'));
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CaseInfoRequest $caseInfoRequest)
     {
-        //
+        try {
+            if (!Gate::allows('dashboard.index')) {
+                return view('errors.403');
+            }
+            // Validate incoming requests
+            $caseInfoData             = $caseInfoRequest->validated();
+
+            // Insert data into related services
+            $caseInfoData = $this->caseInfoService->create($caseInfoData);
+
+            // Log the change
+            __activity('Create Case Type', $caseInfoData);
+
+            return redirect()->route('admin.case-info.index')->with('success', 'Case Type create successfully.');
+        } catch (\Throwable $e) {
+            DB::rollBack(); // Rollback the transaction if any exception occurs
+            Log::error('Error in store method', [
+                'error' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+
+            return view('errors.500', ['errorMessage' => $e->getMessage()]);
+        }
     }
+
 
     /**
      * Display the specified resource.
