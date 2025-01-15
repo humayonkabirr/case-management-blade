127554c04ef5c0606044a31d346fcb49c40cd0f0
humayonkabirr <humayonkobir8@gmail.com>
2025-01-15 16:09:43 +0600

    route fixed

--- routes/admin.php
+++ routes/admin.php
@@ -27,6 +27,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', [AuthenticationController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth:sanctum'],'as' => 'admin.'],  function () {
+    
     // Dashboard Route
     Route::resource('/', DashboardController::class);
 
